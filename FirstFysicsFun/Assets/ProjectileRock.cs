b8ab9203078c61c9d3d9ab3865e346a865ff8b1c
Adrian Caple <adrianocaple@gmail.com>
2016-01-07 15:56:52 -0800

    got basic projectile and projectile spawning working

--- FirstFysicsFun/Assets/ProjectileRock.cs
+++ FirstFysicsFun/Assets/ProjectileRock.cs
@@ -1,20 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ProjectileRock : MonoBehaviour {
-
-    public Vector3 dir;
-    public Vector2 intensity;
+public class ProjectileRock : MonoBehaviour
+{
+    private Vector3 dir;
+    private Vector2 intensity;
+    public Rigidbody2D rigidbody;
 
 	// Use this for initialization
 	void Start ()
     {
-	    
+
+
+        dir = new Vector3(10, Random.Range(12, 5), 1);
+        intensity = new Vector2(1.1f, 1.1f);
+        rigidbody.velocity = new Vector3((dir.x * intensity.x), (dir.y * intensity.y), dir.z);
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-	    
-	}
+         Destroy(gameObject, 4);
+    }
 }
