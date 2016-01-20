2d89d2c2f785d69e54f0f27b863e30f97fc55071
Wade Gushikuma <wade.gushikuma@students.aie.edu.au>
2016-01-19 17:50:51 -0800

    added buttons for levels

---
+++ FirstFysicsFun/Assets/Scripts/Level_Manager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class Level_Manager : MonoBehaviour {
+
+	public Button currButton;
+
+	public void LoadLevel()
+	{
+		Application.LoadLevel (1);
+	}
+}
