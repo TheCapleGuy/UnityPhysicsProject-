c95a92daa9d1f1bcc23e07b4d78511c091d5dbd3
Adrian Caple <adrianocaple@gmail.com>
2016-01-28 13:40:48 -0800

    drag.cs now drags and rotates objs after instantiation

--- FirstFysicsFun/Assets/Scripts/drag.cs
+++ FirstFysicsFun/Assets/Scripts/drag.cs
@@ -4,11 +4,71 @@ using System.Collections;
 public class drag : MonoBehaviour {
 
     public int distance = 12;
+    private Camera cam;
+    private bool rotating, wasDragging, isDragging = false;
+    private Quaternion rotation;
 
-	//void OnMouseDrag()
- //   {
- //       Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
- //       Vector3 objPos = Camera.main.ScreenToWorldPoint(mousePos);
- //       this.transform.position = objPos;
- //   }
+    void Start()
+    {
+        cam = FindObjectOfType<Camera>();
+    }
+
+    private Vector3 mousePos, mouseLocInWorldSpace;
+    public void OnMouseDrag()
+    {
+        isDragging = true;
+        this.gameObject.transform.position = mouseLocInWorldSpace;
+    }
+
+    //public void EndDrag()
+    //{ 
+    //    rotating = true;
+    //    Debug.Log(rotating);
+    //}
+
+    public void RotateObj()
+    {
+
+        if (rotating) //selectedObj.transform.LookAt(mouseLocInWorldSpace, transform.up);
+        {
+            this.gameObject.transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            rotating = false;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+            isDragging = false;
+
+        if (wasDragging && !isDragging)
+            rotating = true;
+
+        if(rotating)
+            rotation = Quaternion.LookRotation(
+                                /*Input.mousePosition - selectedObj.transform.position,*/
+                                mouseLocInWorldSpace - this.gameObject.transform.position,
+                                this.gameObject.transform.TransformDirection(Vector3.up));
+
+        mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
+
+        mouseLocInWorldSpace = cam.ScreenToWorldPoint(mousePos);
+
+        RotateObj();
+    }
+
+    void LateUpdate()
+    {
+        wasDragging = isDragging;
+    }
+    //void OnMouseDrag()
+    //   {
+    //       Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+    //       Vector3 objPos = Camera.main.ScreenToWorldPoint(mousePos);
+    //       this.transform.position = objPos;
+    //   }
 }
