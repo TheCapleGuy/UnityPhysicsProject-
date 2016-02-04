0ae90bf86e1df67bc8666d19c5f99410362fc1f5
Adrian Caple <adrianocaple@gmail.com>
2016-02-04 11:51:10 -0800

    modified chainedrock so it looks much better

---
+++ FirstFysicsFun/Assets/Prefabs/Projectiles/ChainedRockLaunch.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChainedRockLaunch : MonoBehaviour {
+    //script for chainedRock prefab
+    //this prefab needs extra velocity on spawn
+    //add random
+    public Vector2 force;
+    public Rigidbody2D rbody;
+    //for making the projectiles not land in the same spot all the time
+    public float randRange;
+    private Vector2 rand;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rand = new Vector2(Random.Range(-randRange, randRange), Random.Range(-randRange, randRange));
+        rbody.AddForce(force + rand, ForceMode2D.Impulse);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
